566f713ee9e37139bc753059a5d2ab5064397ddf
U-SP-ASM\borja.ferrer <borja.ferrer@AFE0803366.sp.asm-corp>
2016-05-30 12:37:40 +0200

    Added springboot as a dependency for delivery Added initial structure based en DDD Added ignore files from IDE and build folder

---
+++ src/main/java/getes/jobservicemanager/prueba.java
@@ -0,0 +1,11 @@
+package getes.jobservicemanager;
+
+/**
+ * Created by borja.ferrer on 29/04/2016.
+ */
+public class prueba {
+
+    public static void main(String[] args) {
+        System.out.println("lalala");
+    }
+}
